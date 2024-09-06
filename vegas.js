c7a4e703cb4735888d2f1c5bc212da806b2d0618
hitopi <150417534+hitopi@users.noreply.github.com>
2024-09-06 13:37:25 +0900

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -1,14 +1,14 @@
 $(function() {
     $('#mainimg').vegas({
         slides: [
-            { src: 'https://via.placeholder.com/800x600' },  // 画像1
-            { src: 'https://via.placeholder.com/800x600?text=Slide2' },  // 画像2
-            { src: 'https://via.placeholder.com/800x600?text=Slide3' }   // 画像3
+            { src: 'https://github.com/hitopi/summer-festival-2024/raw/main/%E3%82%B9%E3%83%A9%E3%82%A4%E3%83%89%E3%82%B7%E3%83%A7%E3%83%BC1.jpg' },  // 1枚目の写真指定
+            { src: 'https://github.com/hitopi/summer-festival-2024/raw/main/%E3%82%B9%E3%83%A9%E3%82%A4%E3%83%89%E3%82%B7%E3%83%A7%E3%83%BC2.jpg' },  // 2枚目の写真指定
+            { src: 'https://github.com/hitopi/summer-festival-2024/raw/main/%E3%82%B9%E3%83%A9%E3%82%A4%E3%83%89%E3%82%B7%E3%83%A7%E3%83%BC3.jpg' },  // 3枚目の写真指定
         ],
-        transition: 'blur',
-        animation: 'kenburns',
-        delay: 6000,
-        animationDuration: 10000,
-        timer: false
+        transition: 'blur',             // 切り替え時のトランジション
+        animation: 'kenburns',           // スライド中のアニメーション
+        delay: 6000,                     // 次の画像に切り替わるまでの時間 (ミリ秒)
+        animationDuration: 10000,        // 各アニメーションの表示時間
+        timer: false                     // プログレスバーを非表示に
     });
 });
