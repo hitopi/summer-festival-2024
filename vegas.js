24bf0dcf6adc8bd67f1ccedde13917a863d4f8a7
hitopi <150417534+hitopi@users.noreply.github.com>
2024-09-06 13:15:34 +0900

    Update vegas.js

--- vegas.js
+++ vegas.js
@@ -1,14 +1,16 @@
 $(function() {
     $('#mainimg').vegas({
         slides: [
-            { src: './images/1.jpg' },	//1枚目の写真指定
-            { src: './images/2.jpg' },	//2枚目の写真指定
-            { src: './images/3.jpg' },	//3枚目の写真指定
+            { src: './images/1.jpg' },  // 1枚目の写真指定
+            { src: './images/2.jpg' },  // 2枚目の写真指定
+            { src: './images/3.jpg' },  // 3枚目の写真指定
+            { src: './images/4.jpg' },  // 4枚目の写真追加
+            { src: './images/5.jpg' },  // 5枚目の写真追加
         ],
-		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
-		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
-		delay: 6000,				//次の画像を表示するまでの時間
-		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
-		timer: false,				//プログレスバーを非表示に。
+        transition: 'blur',             // 切り替え時のトランジション
+        animation: 'kenburns',           // スライド中のアニメーション
+        delay: 6000,                     // 次の画像に切り替わるまでの時間 (ミリ秒)
+        animationDuration: 10000,        // 各アニメーションの表示時間
+        timer: false                     // プログレスバーを非表示に
     });
 });
